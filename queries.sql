88f054b668327b0c860685e1f83d17bd1e82ba93
JillianJaclyn <jillian.fussner@gmail.com>
2023-12-01 22:33:35 -0600

    Test 1, 2, 3, & 4 Passing

--- queries.sql
+++ queries.sql
@@ -9,6 +9,6 @@ SELECT name FROM employer WHERE location = "St. Louis City";
 DROP TABLE job;
 --Part 4
 SELECT * FROM skill
-(LEFT|INNER) JOIN job_skills ON (skill.id = job_skills.skills_id OR job_skills.skills_id = skill.id)
+LEFT JOIN job_skills ON skill.id = job_skills.skills_id
 WHERE job_skills.jobs_id IS NOT NULL
 ORDER BY name ASC;
